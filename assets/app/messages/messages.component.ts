cf4eee7782763aff30a848df9e8f0a5da83c62e8
Aaron Cremona <dubbacremona@gmail.com>
2016-11-26 10:53:02 -0700

    break out message  component

---
+++ assets/app/messages/messages.component.ts
@@ -0,0 +1,23 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-message',
+  templateUrl: './messages.component.html',
+  styles: [`
+    .author {
+      display: inline-block;
+      font-style: italic;
+      font-size: 12px;
+      width: 80%;
+    }
+    .config {
+      display: inline-block;
+      text-align: right;
+      font-size: 12px;
+      width: 19%;
+    }
+  `]
+})
+export class MessageComponent {
+
+}
