4cbd677d5c2454eb37cf67f1295a45ff874ddec9
Aaron Cremona <dubbacremona@gmail.com>
2016-11-26 14:54:03 -0700

    added input and list components

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
-import { Message } from './messages/message.model';
 
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html'
 })
 export class AppComponent {
-    messages: Message[] = [
-      new Message('Totally a message', 'F Scott Fitzgerald'),
-      new Message('This is totally another different message', 'F Scott is a Hack'),
-      new Message('Whhaaassssuppp. New message man!', 'homeboy')
-    ];
+
 }
