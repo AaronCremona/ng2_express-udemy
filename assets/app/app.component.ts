9f53f9bf03a63ae2164a33a16b84bf43bb6f202b
Aaron Cremona <dubbacremona@gmail.com>
2016-11-26 11:01:45 -0700

    pass info into the component using property binding

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
+import { Message } from './messages/message.model';
 
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html'
 })
 export class AppComponent {
-    message = {
-      content: "totally a sweet message here",
-      author: "Aaron"
-    }
+    message: Message = new Message('Totally a message', 'F Scott Fitzgerald');
 }
