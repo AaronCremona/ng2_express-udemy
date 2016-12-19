28d6289e152398c8405ff59b65bb47e7aa2f7cae
Aaron Cremona <dubbacremona@gmail.com>
2016-12-19 10:46:20 -0700

    refactored auth to module and added lazy loading

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from "@angular/forms";
 import { HttpModule } from "@angular/http";
 
 import { AppComponent } from "./app.component";
 import { AuthenticationComponent } from "./auth/authentication.component";
 import { HeaderComponent } from "./header.component";
-import { LogoutComponent } from "./auth/logout.component";
-import { SigninComponent } from "./auth/signin.component";
-import { SignupComponent } from "./auth/signup.component";
 import { AuthService } from "./auth/auth.service";
 import { ErrorComponent } from "./errors/error.component";
 import { ErrorService } from "./errors/error.service";
@@ -21,15 +17,11 @@ import { routing } from "./app.routing";
         AppComponent,
         AuthenticationComponent,
         HeaderComponent,
-        LogoutComponent,
-        SigninComponent,
-        SignupComponent,
         ErrorComponent
     ],
     imports: [
       BrowserModule,
       routing,
-      ReactiveFormsModule,
       HttpModule,
       MessageModule
     ],
